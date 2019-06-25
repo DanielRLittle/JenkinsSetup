927009544a56cddb3aff53e40312f17a9477f583
Daniel Little <daniel.r.little.1994@gmail.com>
2019-06-25 17:00:51 +0100

    slight change

--- removeJenkins.sh
+++ removeJenkins.sh
@@ -5,7 +5,9 @@ sudo systemctl disable jenkins
 
 sudo systemctl stop jenkins
 
-sudo userdel -fr jenkins
+sudo userdel -f jenkins
+
+sudo rm -rf /home/jenkins
 
 sudo rm /etc/systemd/system/jenkins.service
 
