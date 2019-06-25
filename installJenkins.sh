589944ac3a0ad98a778fccf88c03b5f3d1076e0f
Daniel Little <daniel.r.little.1994@gmail.com>
2019-06-25 15:34:49 +0100

    altered install jenins and added a new file

--- installJenkins.sh
+++ installJenkins.sh
@@ -9,20 +9,7 @@ sudo usermod --shell /bin/bash jenkins
 
 sudo "su - jenkins; wget http://updates.jenkins-ci.org/latest/jenkins.war"
 
-configureScript() {
-	echo "[Unit]"
-	echo "Description=Jenkins"
-	echo ""
-	echo "[Service]"
-	echo "User=Jenkins"
-	echo "WorkingDirectory=/home/jenkins"
-	echo "ExecStart=/usr/bin/java -jar /home/jenkins/jenkins.war"
-	echo ""
-	echo "[Install]"
-	echo "WantedBy=multi-user.target"
-}
-
-sudo configurescript>/etc/systemd/system/jenkins.service
+sudo mv jenkins.service /etc/systemd/system/jenkins.service
 
 sudo systemctl daemon-reload
 
